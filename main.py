b95c910b882440384b48d30c7fa9a77ec3711bcf
parsa <parsamesg@gmail.com>
2021-08-14 08:41:57 +0100

    create button that adds items based on sample data

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, redirect
 import firestore
 
 app = Flask(__name__)
@@ -10,6 +10,14 @@ def list():
     items = firestore.show_list()
 
     return render_template('list.html', items=items)
+
+
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+
+    firestore.add_item('banana', 5)
+
+    return redirect(url_for('list'))
 
 
 if __name__ == '__main__':
