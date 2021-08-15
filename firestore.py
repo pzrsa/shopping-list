31c00d2a7c2fe21a3d4661fb5257c4fa8821da27
parsa <parsamesg@gmail.com>
2021-08-15 20:46:34 +0100

    move item class into its own file for cleanliness

--- firestore.py
+++ firestore.py
@@ -1,24 +1,6 @@
 from datetime import datetime
 from google.cloud import firestore
-
-
-class Item:
-    """"Simple representation of an item."""
-
-    def __init__(self, name, quantity):
-        self.name = name
-        self.quantity = quantity
-        self.date_created = datetime.now()
-
-    def to_dict(self):
-
-        item = {
-            'name': self.name.title(),
-            'quantity': self.quantity,
-            'date_created': self.date_created
-        }
-
-        return item
+from item import Item
 
 
 def show_list(user_email):
---
+++ item.py
@@ -0,0 +1,20 @@
+import datetime
+
+
+class Item:
+    """"Simple representation of an item."""
+
+    def __init__(self, name, quantity):
+        self.name = name
+        self.quantity = quantity
+        self.date_created = datetime.now()
+
+    def to_dict(self):
+
+        item = {
+            'name': self.name.title(),
+            'quantity': self.quantity,
+            'date_created': self.date_created
+        }
+
+        return item
