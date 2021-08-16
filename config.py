51e5078b7faa2d09fbd234d0886cb397a4f2d334
parsa <parsamesg@gmail.com>
2021-08-16 20:34:42 +0100

    secret key being random on the fly raised CSRF errors

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 
 class Config:
-    SECRET_KEY = os.urandom(16).hex()
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     GOOGLE_CLIENT_ID = os.environ.get('GOOGLE_CLIENT_ID')
     GOOGLE_CLIENT_SECRET = os.environ.get('GOOGLE_CLIENT_SECRET')
     GOOGLE_DISCOVERY_URL = 'https://accounts.google.com/.well-known/openid-configuration'
